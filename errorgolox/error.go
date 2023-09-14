f5993e8f7decf92514bf9ba69889440e1bf7577a
haritsrizkall <haritsrizkal@mail.ugm.ac.id>
2023-09-14 20:32:31 +0700

    match token

---
+++ errorgolox/error.go
@@ -0,0 +1,16 @@
+package errorgolox
+
+import "fmt"
+
+var HadError = false
+
+func LogError(line int, message string) {
+    Report(line, "", message)
+}
+
+func Report(line int, where, message string) {
+    fmt.Errorf("[line %d] Error %s: %s", line, where, message)
+    HadError = true
+}
+
+
--- scanner/scanner.go
+++ scanner/scanner.go
@@ -1,6 +1,9 @@
 package scanner
 
-import "github.com/haritsrizkall/golox/token"
+import (
+	"github.com/haritsrizkall/golox/errorgolox"
+	"github.com/haritsrizkall/golox/token"
+)
 
 type Scanner struct {
     source string
@@ -14,11 +17,12 @@ func (s *Scanner) scanTokens() []token.Token {
     return s.tokens
 }
 
-func (s *Scanner) isAtEnd() int {
-   return 0
+func (s *Scanner) isAtEnd() bool {
+    return s.current >= len(s.source)
 }
 
 func (s *Scanner) scanToken() {
+    s.current++
     c := s.source[s.current]
     switch c {
     case '(':
@@ -41,9 +45,46 @@ func (s *Scanner) scanToken() {
         s.addToken(token.SEMICOLON)
     case '*':
         s.addToken(token.STAR)
+    case '!':
+        if s.match('=') {
+            s.addToken(token.BANG_EQUAL)
+        }else {
+            s.addToken(token.EQUAL_EQUAL)
+        }
+    case '=':
+        if s.match('=') {
+            s.addToken(token.EQUAL_EQUAL)
+        }else {
+            s.addToken(token.EQUAL)
+        }
+    case '<':
+        if s.match('=') {
+            s.addToken(token.LESS_EQUAL)
+        }else {
+            s.addToken(token.LESS)
+        }
+    case '>':
+        if s.match('=') {
+            s.addToken(token.GREATER_EQUAL)
+        }else {
+            s.addToken(token.GREATER)
+        }
+    default:
+        errorgolox.LogError(s.line, "Unexpected char dude")
     }
 }
 
+func (s *Scanner) match(char byte) bool {
+    if !s.isAtEnd() {
+        return false
+    }
+    next := s.current + 1
+    if s.source[next] != char {
+        return false 
+    }
+    s.current++
+    return true
+}
 func (s *Scanner) addToken(tokenType string) {
     s.appendToken(tokenType, nil)
 }
