19dbf634fae1dffd2de65a97bc3a4c0c6f79ee36
haritsrizkall <haritsrizkal@mail.ugm.ac.id>
2023-11-12 14:03:36 +0700

    keywords map

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -7,6 +7,27 @@ import (
 	"github.com/haritsrizkall/golox/token"
 )
 
+var (
+  keywords = map[string]string{
+    "and": token.AND,
+    "class": token.CLASS,
+    "else": token.ELSE,
+    "false": token.FALSE,
+    "for": token.FOR,
+    "fun": token.FUN,
+    "if": token.IF,
+    "nil": token.NIL,
+    "or": token.OR,
+    "print": token.PRINT,
+    "return": token.RETURN,
+    "super": token.SUPER,
+    "this": token.THIS,
+    "true": token.TRUE,
+    "var": token.VAR,
+    "while": token.WHILE,
+  }
+)
+
 type Scanner struct {
     source string
     tokens []token.Token 
